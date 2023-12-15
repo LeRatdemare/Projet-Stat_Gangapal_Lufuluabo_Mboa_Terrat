409dc9628225727e6c4e443340acd37f8707e2aa
Joanne Gangapal <jgangapal@bordeaux-inp.fr>
2023-12-15 16:16:13 +0100

    Début ACP par Bryan

--- Projet_Stat.R
+++ Projet_Stat.R
@@ -1,4 +1,16 @@
 ########## ZONE BRYAN
+donnees <- readRDS("H:/AAB Projets/projet-stat-2A-2023-2024/activation.Rdata")
+attach(donnees)
+
+#Analyse de stats descriptive
+summary(donnees)
+
+library(PCAmixdata)
+acp = PCAmix(donnees[,-c(1,2)])
+
+sexe = as.factor(donnees$Sexe)
+plot(acp,choice = "ind",coloring.ind = sexe)
+
 
 
 ########## ZONE JOANNE
